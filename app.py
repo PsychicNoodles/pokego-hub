4bf7557423150095037f5fe56f92e25c24278b03
Birnbaum, Mattori <Birnbaum.Mattori@principal.com>
2016-07-21 15:29:47 -0500

    added reading and printing all relevant fields from map objects

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import logging
 import argparse
 import time
 import json
+from enum import IntEnum
 
 from pgoapi import PGoApi
 from pgoapi.utilities import f2i, h2f
@@ -16,6 +17,16 @@ from s2sphere import CellId, LatLng
 
 log = logging.getLogger(__name__)
 
+class FortType(IntEnum):
+    gym = 0
+    stop = 1
+
+class Teams(IntEnum):
+    neutral = 0
+    blue = 1
+    red = 2
+    yellow = 3
+
 def get_pos_by_name(location_name, proxy):
     geolocator = GoogleV3(proxies=proxy, timeout=5)
     loc = geolocator.geocode(location_name)
@@ -25,19 +36,21 @@ def get_pos_by_name(location_name, proxy):
 
     return (loc.latitude, loc.longitude, loc.altitude)
 
-def get_cellid(lat, long):
+def get_cell_ids(lat, long, radius = 10):
     origin = CellId.from_lat_lng(LatLng.from_degrees(lat, long)).parent(15)
     walk = [origin.id()]
+    right = origin.next()
+    left = origin.prev()
 
-    # 10 before and 10 after
-    next = origin.next()
-    prev = origin.prev()
-    for i in range(10):
-        walk.append(prev.id())
-        walk.append(next.id())
-        next = next.next()
-        prev = prev.prev()
-    return ''.join(map(encode, sorted(walk)))
+    # Search around provided radius
+    for i in range(radius):
+        walk.append(right.id())
+        walk.append(left.id())
+        right = right.next()
+        left = left.prev()
+
+    # Return everything
+    return sorted(walk)
 
 def encode(cellid):
     output = []
@@ -120,13 +133,98 @@ def main():
         log.info("failed to login, exiting")
         return
 
+    log.debug("getting cell ids")
+    cell_ids = get_cell_ids(position[0], position[1])
+    log.debug("cell ids are: %s" % cell_ids)
+
     response = api.get_player() \
                   .get_map_objects(latitude=f2i(position[0]), longitude=f2i(position[1]),
-                                   since_timestamp_ms=time.time() * 1000,
-                                   cell_id=get_cellid(position[0], position[1])) \
+                                   since_timestamp_ms=[0,] * len(cell_ids),
+                                   cell_id=cell_ids) \
                   .call()
 
-    print "Response dictionary: \n\r%s" % json.dumps(response, indent=2)
+    pokemen, gyms, stops, spawns = [], [], [], []
+    if 'status' in response['responses']['GET_MAP_OBJECTS'] and \
+    response['responses']['GET_MAP_OBJECTS']['status'] is 1:
+        for cell in response['responses']['GET_MAP_OBJECTS']['map_cells']:
+            if 'wild_pokemons' in cell:
+                for pokeman in cell['wild_pokemons']:
+                    pokemen.append({
+                        'id': pokeman['encounter_id'],
+                        'spawnpoint': pokeman['spawnpoint_id'],
+                        'lat': pokeman['latitude'],
+                        'lng': pokeman['longitude'],
+                        'pokeid': pokeman['pokemon_data']['pokemon_id'],
+                        'disappears': time.time() + pokeman['time_till_hidden_ms'] / 1000,
+                        'last_mod': pokeman['last_modified_timestamp_ms'] # dunno what this does
+                    })
+            if 'forts' in cell:
+                for fort in cell['forts']:
+                    f = {
+                        'id': fort['id'],
+                        'lat': fort['latitude'],
+                        'lng': fort['longitude'],
+                        'type': FortType(fort.get('type', 0)), # gyms are type 0, but aren't listed
+                        'enabled': fort['enabled'], # dunno what this does
+                        'last_mod': fort['last_modified_timestamp_ms'] # dunno what this does
+                    }
+                    if f['type'] is FortType.gym:
+                        f.update({
+                            'points': fort['gym_points'],
+                            'guard_pokeid': fort['guard_pokemon_id'],
+                            'team': Teams(fort.get('owned_by_team', 0))
+                        })
+                        gyms.append(f)
+                    else:
+                        stops.append(f)
+            if 'spawn_points' in cell:
+                for spawn in cell['spawn_points']:
+                    spawns.append({
+                        'lat': spawn['latitude'],
+                        'lng': spawn['longitude'],
+                        'decimated': False
+                    })
+            if 'decimated_spawn_points' in cell:
+                for spawn in cell['decimated_spawn_points']:
+                    spawns.append({
+                        'lat': spawn['latitude'],
+                        'lng': spawn['longitude'],
+                        'decimated': True
+                    })
+    print('Pokemen: ', pokemen)
+    print('Gyms: ', gyms)
+    print('Stops: ', stops)
+    print('Spawns: ', spawns)
+
+    # def removeDeci(d):
+    #     d.pop('decimated')
+    #     return d
+    # print('All spawns are unique: ', len([dict(y) for y in set(tuple(x.items()) for x in map(removeDeci, spawns))]) == len(spawns))
+
+
+def generate_spiral(starting_lat, starting_lng, step_size, step_limit):
+    coords = [{'lat': starting_lat, 'lng': starting_lng}]
+    steps,x,y,d,m = 1, 0, 0, 1, 1
+    rlow = 0.0
+    rhigh = 0.0005
+
+    while steps < step_limit:
+        while 2 * x * d < m and steps < step_limit:
+            x = x + d
+            steps += 1
+            lat = x * step_size + starting_lat + random.uniform(rlow, rhigh)
+            lng = y * step_size + starting_lng + random.uniform(rlow, rhigh)
+            coords.append({'lat': lat, 'lng': lng})
+        while 2 * y * d < m and steps < step_limit:
+            y = y + d
+            steps += 1
+            lat = x * step_size + starting_lat + random.uniform(rlow, rhigh)
+            lng = y * step_size + starting_lng + random.uniform(rlow, rhigh)
+            coords.append({'lat': lat, 'lng': lng})
+
+        d = -1 * d
+        m = m + 1
+    return coords
 
 if __name__ == '__main__':
     main()
